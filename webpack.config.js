e175fc5b8380f131df370ccdff38e61b669adeac
Anton Agaronov <anton.agaronov@legalbet.com>
2023-06-05 10:57:53 +0400

    fix webpack.config.js by eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
@@ -8,94 +8,83 @@ const EsLintPlugin = require('eslint-webpack-plugin');
 const isProd = process.env.NODE_ENV === 'production';
 const isDev = !isProd;
 
-const filename = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`;
+const filename = (ext) => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`;
 
-const jsLoaders = () => {
-    const loaders = [
-        {
-            loader: "babel-loader",
-            options: {
-                presets: ['@babel/preset-env']
-            }
-        }
-    ]
-
-    if (isDev) {
-
-    }
-
-    return loaders;
-}
-
-console.log('IS PROD', isProd)
-console.log('IS DEV', isDev)
+console.log('IS PROD', isProd);
+console.log('IS DEV', isDev);
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
   mode: 'development',
   entry: './index.js',
   output: {
-    //Хеш нужен для того, чтобы браузер всегда скачивал актуальную версию бандла, так как каждый раз будет новое название файла, а не будет доставать из кеша одну и туже копию
+    // Хеш нужен для того, чтобы браузер всегда скачивал актуальную версию бандла,
+    // так как каждый раз будет новое название файла, а не будет доставать из кеша одну и туже копию
     filename: filename('js'),
     path: path.resolve(__dirname, 'dist'),
   },
-    resolve: {
-        extensions: ['.js'],
-        alias: {
-            '@': path.resolve(__dirname, 'src'),
-            '@core': path.resolve(__dirname, 'src/core'),
-        }
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@core': path.resolve(__dirname, 'src/core'),
     },
+  },
   plugins: [
-      new EsLintPlugin(),
-      new CleanWebpackPlugin(),
-      new HtmlWebpackPlugin({
+    new EsLintPlugin(),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
       template: './index.html',
       minify: {
-          collapseWhitespace: isProd,
-          removeComments: isProd,
-      }
-      }),
-      new CopyPlugin({
-        patterns: [
+        collapseWhitespace: isProd,
+        removeComments: isProd,
+      },
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/favicon3.ico'),
+          to: path.resolve(__dirname, 'dist'),
+        },
+      ],
+    }),
+    new MiniCssExtractPlugin({
+      filename: filename('css'),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
           {
-            from: path.resolve(__dirname, 'src/favicon3.ico'),
-            to: path.resolve(__dirname, 'dist'),
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass-embedded'),
+              sassOptions: {
+                fiber: false,
+              },
+            },
           },
         ],
-      }),
-      new MiniCssExtractPlugin({
-          filename: filename('css')
-      })
-  ],
-    module: {
-        rules: [
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            implementation: require('sass-embedded'),
-                            sassOptions: {
-                                fiber: false,
-                            },
-                        },
-                    },
-                ],
-            },
-            {
-                test: /\.m?js$/,
-                exclude: /node_modules/,
-                use: jsLoaders(),
-            }
-        ],
-    },
-    devtool: isDev ? 'source-map' : false,
-    devServer: {
-      port: 3000,
-      hot: isDev,
-    }
-}
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  devtool: isDev ? 'source-map' : false,
+  devServer: {
+    port: 3000,
+    hot: isDev,
+  },
+};
